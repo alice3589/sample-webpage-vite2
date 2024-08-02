7d19a24165f333f5e63aeb9a191385b709d39e2c
alice3589 <ryutoiwa2929@gmail.com>
2024-08-02 17:04:18 +0900

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: true
+  },
+  base: '/sample-webpage-vite2/',
+  build: {
+    outDir: 'docs',
+  },
+  plugins: [react()],
+})
